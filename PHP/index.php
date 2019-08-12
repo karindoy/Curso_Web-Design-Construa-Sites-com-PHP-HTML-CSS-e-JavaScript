b90d70d39c6a2f8c7a20477b1d5caedfa57bbcec
karin doy <doykarin@gmail.com>
2019-08-11 23:44:37 -0300

    inicio php aula 50

---
+++ PHP/index.php
@@ -0,0 +1,21 @@
+<?php echo 'eai'; ?>
+
+<?php  $mensagem = 'olaaaa '; 
+
+echo $mensagem;
+
+$a= 2;
+$b= 3;
+
+$soma = $a ** $b;
+print $soma;
+
+$c= 5;
+$d= 6;
+
+if ($d % 2 == 0){
+    echo 'numero par';
+}else{
+echo 'numero impar';
+}
+?>
